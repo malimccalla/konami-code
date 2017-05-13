d0dd0ac8cbb7ee86445127cdd36062ac65955d79
Mali Michael <malimccalla@gmail.com>
2017-05-13 14:22:07 +0100

    Export nameless function by default and change Keys to KonamiCode

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-module.exports = KonamiCode = (userEvent) => {
-  const keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+module.exports = (userEvent) => {
+  const KonamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
   let userSequence = [];
   let position = 0;
   document.addEventListener('keydown', (e) => {
-    if (e.keyCode !== keys[position]) {
+    if (e.keyCode !== KonamiCode[position]) {
       userSequence = [];
       position = 0;
     } else {
       userSequence.push(e.keyCode);
       position += 1;
-      if (userSequence === keys) {
+      if (userSequence.length === 10) {
         userEvent();
         userSequence = [];
         position = 0;
       }
     }
-  })
+  });
 };
